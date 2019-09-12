aa6b2571066a8343c373c2597b5d2dabbf550717
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-09-12 13:14:21 +0900

    Vuetify/pugの追加

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+import vuetify from "./plugins/vuetify"
 import "./registerServiceWorker"
 
 Vue.config.productionTip = false
@@ -9,6 +10,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   render: (h): any => h(App)
 }).$mount("#app")
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,19 @@
+import Vue from "vue"
+import Vuetify from "vuetify"
+import "vuetify/dist/vuetify.min.css"
+import ja from "vuetify/src/locale/ja"
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  icons: {
+    iconfont: "md" || "mdi"
+  },
+  theme: {
+    // dark: true
+  },
+  lang: {
+    locales: { ja },
+    current: "ja"
+  }
+})
