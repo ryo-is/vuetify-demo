cf00c553a495788d77c89f48ddab18a2d42776ce
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-09-12 16:59:36 +0900

    feat:font-familyにフォントを追加

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Avenir", "Helvetica Neue", "Helvetica", "Arial", "Hiragino Sans",
+    "ヒラギノ角ゴシック", YuGothic, "Yu Gothic", "メイリオ", Meiryo,
+    "ＭＳ Ｐゴシック", "MS PGothic";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
