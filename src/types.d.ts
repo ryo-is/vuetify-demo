f85d0c24f60555a3f1f6306178e6167cfc1e7a7d
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-10-03 17:51:37 +0900

    feat:Vuetifyのデモ拡充

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,14 @@
 export type HomeComponentState = {
   text: string
   dialog: boolean
+  tableHeaders: DataTableHeaderType[]
+  tableItems: Array<{ [k: string]: string | number }>
+  selectDate: string
+}
+
+export type DataTableHeaderType = {
+  text: string
+  value: string
+  align?: string
+  sortable?: boolean
 }
--- src/views/home/script.ts
+++ src/views/home/script.ts
@@ -1,12 +1,27 @@
 import Vue from "vue"
 import { HomeComponentState } from "@/types"
+import dayjs from "dayjs"
 
 export default Vue.extend({
   data(): HomeComponentState {
     return {
       text: "Hello Vue!!!",
-      dialog: true
+      dialog: false,
+      tableHeaders: [
+        { text: "Name", value: "name", sortable: false },
+        { text: "Age", value: "age" },
+        { text: "Gender", value: "gender", sortable: false }
+      ],
+      tableItems: [
+        { name: "Taro", age: 20, gender: "Male" },
+        { name: "Hanako", age: 18, gender: "Female" },
+        { name: "Ichiro", age: 25, gender: "Male" }
+      ],
+      selectDate: ""
     }
+  },
+  mounted() {
+    this.selectDate = dayjs().format("YYYY-MM-DD")
   },
   methods: {
     getText(): string {
